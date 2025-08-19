9de1d699ea9c20c3d9ddad3682d635854321a50d
Tomaz Zlindra <tomz0840@gmail.com>
2025-08-19 14:12:15 -0700

    Added ESP32 Stretcher Config

--- boards/xtensa/esp32/esp32-devkitc/src/esp32_gpio.c
+++ boards/xtensa/esp32/esp32-devkitc/src/esp32_gpio.c
@@ -55,8 +55,8 @@
  */
 
 #define I2C_MUX_0 25 // associated with /dev/gpio0 (MUST BE IN THIS ORDER)
-// #define I2C_MUX_1 26 // associated with /dev/gpio1 (MUST BE IN THIS ORDER)
-// #define I2C_MUX_2 0 // associated with /dev/gpio2 (MUST BE IN THIS ORDER)
+#define I2C_MUX_1 26 // associated with /dev/gpio1 (MUST BE IN THIS ORDER)
+#define I2C_MUX_2 0 // associated with /dev/gpio2 (MUST BE IN THIS ORDER)
 
 /* Input pins. GPIO18 is used as an example, any other inputs could be
  * used.
@@ -123,9 +123,9 @@ static const struct gpio_operations_s gpout_ops =
 
 static const uint32_t g_gpiooutputs[BOARD_NGPIOOUT] =
 {
-  I2C_MUX_0
-  // I2C_MUX_1,
-  // I2C_MUX_2
+  I2C_MUX_0,
+  I2C_MUX_1,
+  I2C_MUX_2
 };
 
 static struct esp32gpio_dev_s g_gpout[BOARD_NGPIOOUT];
